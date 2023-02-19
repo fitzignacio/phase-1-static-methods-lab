d4921510273fceae5587512f4916f2583c8fe96f
Fitz Ignacio <ignacio.fitz@gmail.com>
2023-02-18 20:30:44 -0800

    commit tree dd5643d96bab0ad9f1ab584f0f2889f1c50b083c

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 class Formatter {
   //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^a-zA-Z0-9-' ]+/g, '');
+  }
+
+  static titleize(string) {
+    const exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    const words = string.split(' ');
+
+    for (let i = 0; i < words.length; i++) {
+      if (i === 0) {
+        words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);
+      } else {
+        if (!exceptions.includes(words[i])) {
+          words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);
+        }
+      }
+    }
+
+    return words.join(' ');
+  }
 }
